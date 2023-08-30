a2270bc10c62ba705a63c0131934daf3cd7ab5a0
Ana Cecília <anaceciliabatalha@gmail.com>
2023-08-30 15:17:34 -0400

    Primeiro controller e testes nas páginas Razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,9 +17,11 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
+            // method builder
         }
 
         public IActionResult Contact()
